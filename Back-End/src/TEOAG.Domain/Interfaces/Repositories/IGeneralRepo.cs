a64c58062a472d997d0ac0d830430a3ecf9f9601
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-27 15:40:38 -0300

    add: interfaces

--- Back-End/src/TEOAG.Domain/Interfaces/Repositories/IGeneralRepo.cs
+++ Back-End/src/TEOAG.Domain/Interfaces/Repositories/IGeneralRepo.cs
@@ -1,7 +1,17 @@
+using System.Threading.Tasks;
+
 namespace TEOAG.Domain.Interfaces.Repositories
 {
     public interface IGeneralRepo
     {
-         
+         void Adicionar<T>(T entity) where T : class;
+
+         void Atualizar<T>(T entity) where T : class;
+
+         void Deletar<T>(T entity) where T : class;
+
+         void DeletarVarias<T>(T[] entity) where T : class;
+
+         Task<bool> SalvarMudancasAsync();
     }
 }
--- Back-End/src/TEOAG.Domain/Interfaces/Repositories/IProductRepo.cs
+++ Back-End/src/TEOAG.Domain/Interfaces/Repositories/IProductRepo.cs
@@ -1,7 +1,12 @@
+using System.Threading.Tasks;
+using TEOAG.Domain.Entities;
+
 namespace TEOAG.Domain.Interfaces.Repositories
 {
     public interface IProductRepo
     {
-         
+         Task<Product[]> PegaTodasAsync();
+         Task<Product> PegaPorIdAsync();
+         Task<Product> PegaPorTituloAsync();
     }
 }
